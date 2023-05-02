6a5f8782d8ef10da85d7438315c4db6a8133ed79
Noman Shaikh <noman.shaikh@neosoftmail.com>
2023-05-02 09:47:45 +0530

    WOrking on preview screen

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,22 +1,46 @@
 import React, { useEffect } from "react";
 import "./Preview.css";
-import { selectCameraImage } from "../../features/cameraSlice";
-import { useSelector } from "react-redux";
+import {
+  resetCameraImage,
+  selectCameraImage,
+} from "../../features/cameraSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import {
+  AttachFile,
+  Close,
+  Create,
+  Crop,
+  MusicNote,
+  TextFields,
+  Timer,
+} from "@mui/icons-material";
 
 const Preview = () => {
   const cameraImage = useSelector(selectCameraImage);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   // console.log("cameraImage", cameraImage);
   useEffect(() => {
     if (!cameraImage) {
-      navigate("/");
+      navigate("/", { replace: true });
     }
   }, [cameraImage, navigate]);
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+  };
   return (
     <div className="preview">
-      <h1>Preview</h1>
+      <Close className="preview_close" onClick={closePreview} />
+      <div className="preview_toolbarRight">
+        <TextFields />
+        <Create />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <Timer />
+      </div>
       <img src={cameraImage} alt="preview_of_image" />
     </div>
   );
